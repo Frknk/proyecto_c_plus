aad516a4def8d71090828042df70df11a84a4c7b
Frknk <frknk1@protonmail.com>
2023-05-16 17:09:20 -0500

    Arreglando (16-05)

--- jugueteria.cpp
+++ jugueteria.cpp
@@ -12,15 +12,27 @@ private:
     string marca;
 
 public:
-    Jugueteria(string _id, float _precio, string _nombre, string _tipo, string _marca) : Producto(_id, _precio, _nombre)
+    Jugueteria(string _id, float _precio, string _nombre, string _tipo, string _marca, bool _educativo) : Producto(_id, _precio, _nombre)
     {
-
         marca = _marca;
         tipo = _tipo;
+        educativo = _educativo;
+    }
+
+    string getEducativoString()
+    {
+        if (educativo)
+        {
+            return "Si";
+        }
+        else
+        {
+            return "No";
+        }
     }
 
     string getDatos()
     {
-        return "ID: " + getId() + " | Precio: " + getPrecioString() + "\n" + "Nombre: " + nombre + " - Tipo: " + tipo + " - Marca: " + marca;
+        return "ID: " + getId() + " | Precio: " + getPrecioString() + "\n" + "Nombre: " + nombre + " - Tipo: " + tipo + " - Marca: " + marca + " - Es educativo: " + getEducativoString();
     }
 };
