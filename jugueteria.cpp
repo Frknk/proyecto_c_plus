a0b605922307a4465ccf351e515ee3c9980d99f9
Frknk <frknk1@protonmail.com>
2023-05-16 17:07:13 -0500

    Arreglando (16-05)

--- jugueteria.cpp
+++ jugueteria.cpp
@@ -1,30 +1,26 @@
-#include<iostream>
-#include<string>
-#include"producto.cpp"
+#include <iostream>
+#include <string>
+#include "producto.cpp"
 
 using namespace std;
 
-class Electronica:public Producto{
-private: 
-
-string cantidad_producto;
-string tipo;
-string nombre_del_producto;
-bool educativo;
-string marca;
+class Jugueteria : public Producto
+{
+private:
+    string tipo;
+    bool educativo;
+    string marca;
 
 public:
-Electronica( string _cantidad_producto, string _tipo , string _marca, string _nombre_del_producto):Producto(_id,_precio){
+    Jugueteria(string _id, float _precio, string _nombre, string _tipo, string _marca) : Producto(_id, _precio, _nombre)
+    {
 
-cantidad_producto=_cantidad_producto;
-marca=_marca;
-tipo=_tipo;
-nombre_del_producto=_nombre_del_producto;
+        marca = _marca;
+        tipo = _tipo;
+    }
 
-}
-
-string getDatos(){
-return "ID: " + getId() + " | Precio: " + getPrecioString() + "\n" + "nombre del producto"+ nombre_del_producto +"\n"+ "Tipo: "+ tipo + "\n" + " cantidad de producto:" +cantidad_producto +"\n" + "marca del producto:" + marca + "\n"+ ;
-}
-
+    string getDatos()
+    {
+        return "ID: " + getId() + " | Precio: " + getPrecioString() + "\n" + "Nombre: " + nombre + " - Tipo: " + tipo + " - Marca: " + marca;
+    }
 };
