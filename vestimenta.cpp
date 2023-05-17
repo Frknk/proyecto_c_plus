77e4dccea4e005e45a0abb419f87a6187b810565
denpol <polinareugeniod@gmail.com>
2023-05-17 15:54:52 -0500

    modificar clase vestimenta

--- vestimenta.cpp
+++ vestimenta.cpp
@@ -5,54 +5,52 @@ using namespace std;
 
 class Vestimenta {
     private:
-    string nombre_vestimenta;
-    string codigo_vestimenta;
     string talla;
-    string precio_vestimenta;
-    
+    string marca;
+    string color;
+    string material_prenda;
+    string diseño;
+
     public:
-    Vestimenta(string nombre, string codigo, string talla, string precio) {
-        nombre_vestimenta = nombre;
-        codigo_vestimenta = codigo;
-        this->talla = talla;
-        precio_vestimenta = precio;
+    Vestimenta( string _talla, string marca, string _color, string _material_prenda){
+        talla = talla;
     }
-    
-    string getNombreVestimenta() {
-        return nombre_vestimenta;
-    }
-    
-    void setNombreVestimenta(string nombre) {
-        nombre_vestimenta = nombre;
-    }
-    
-    string getCodigoVestimenta() {
-        return codigo_vestimenta;
-    }
-    
-    void setCodigoVestimenta(string codigo) {
-        codigo_vestimenta = codigo;
-    }
-    
+
     string getTalla() {
         return talla;
     }
     
     void setTalla(string talla) {
-        this->talla = talla;
+        talla = talla;
     }
     
-    string getPrecioVestimenta() {
-        return precio_vestimenta;
+    string getMarca() {
+        return marca;
     }
     
-    void setPrecioVestimenta(string precio) {
-        precio_vestimenta = precio;
+    void setMarca(string _marca) {
+        marca = _marca;
+    }
+
+     string getColor() {
+        return color;
+    }
+    
+    void setColor(string _color) {
+        color = _color;
+    }
+
+    string getMaterial_prenda() {
+        return material_prenda;
+    }
+    
+    void setMaterial_prenda(string _Material_prenda) {
+        color = _Material_prenda;
     }
     
     string getDatosvestimenta() {
         string resumen;
-        resumen="Nombre de la vestimenta: "+nombre_vestimenta+"Código de la vestimenta: "+codigo_vestimenta+"Talla: "+talla+"Precio de la vestimenta: "+precio_vestimenta+"\n";
+        resumen="Talla: "+talla+"Marca: "+marca+"Color: "+color+"Material de la prenda: "+material_prenda+"\n";
 
     }
 };
